dec3e429b2f9dde7a30ad3cbd577c79b2e7a41ff
Christian Hanisch <info@tandron.de>
2023-07-12 16:43:16 +0200

    Projektdateien hinzufügen.

---
+++ WpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.ViewModels
+{
+    public class MainViewModel :  BindableBase
+    {
+        public DelegateCommand ConnectingCommand { get; }
+
+        public MainViewModel()
+        {
+
+            ConnectingCommand = new DelegateCommand(ConnectingFunc);
+        }
+
+        private void ConnectingFunc()
+        {
+        }
+    }
+}
